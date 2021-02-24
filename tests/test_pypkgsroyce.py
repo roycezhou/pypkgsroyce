7183fecdb71d83a10c459854e0a186175628d524
Siqi Zhou <nikey.chow@gmail.com>
2021-02-24 13:48:32 -0800

    added unit tests for catbind

--- tests/test_pypkgsroyce.py
+++ tests/test_pypkgsroyce.py
@@ -1,5 +1,14 @@
 from pypkgsroyce import __version__
 from pypkgsroyce import pypkgsroyce
+import pandas as pd
+
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((pypkgsroyce.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((pypkgsroyce.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
